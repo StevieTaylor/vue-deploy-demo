5c38e2f4b048a536451bb5b1dd2d301bb2d6d532
StevieTaylor <galei111@126.com>
2021-09-29 16:16:38 +0800

    [feat] vue config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-09-29 16:14:53
+ * @LastEditTime: 2021-09-29 16:15:59
+ * @LastEditors: Stevie
+ * @Description:
+ */
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-deploy-demo' : '/',
+}
